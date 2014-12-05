b37d5ad630f6993a08bd5e890e06d08659b6c515
Roicel Laya <roicel.laya@gmail.com>
2014-12-05 02:41:26 -0430

    Cambiada la url y la lógica para obtener la lista de cosas (en mi caso tareas).

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -13,45 +13,49 @@
                   timeout:1000,
               });
               //Here you have to change it for your local ip 
-              sendit.open('GET', 'http://192.168.0.120/read_todo_list.php');
+              sendit.open('GET', 'http://localhost:3000/api/models');
               sendit.send();
               //Function to be called upon a successful response 
               sendit.onload = function(){
-                     var json = JSON.parse(this.responseText); 
-                     var json = json.todo; 
+                     var json = JSON.parse(this.responseText);
+                     console.log('El responseText fue: ', this.responseText);
+                     var json = json.message;
+                     console.log('json vale: ', json);
                      //if the database is empty show an alert 
-                     if(json.length == 0){ 
+                     if(json.length == 0){
                             $.tableView.headerTitle = "The database row is empty"; 
                      }                      
-                     //Emptying the data to refresh the view 
-                     dataArray = [];                      
-                     //Insert the JSON data to the table view 
-                     for( var i=0; i<json.length; i++){ 
-                           var row = Ti.UI.createTableViewRow({ 
-                                  title: json[i].todo, 
-                                  hasChild : true, 
-                           });        
-                         dataArray.push(row);                 
+                     //Emptying the data to refresh the view
+                     dataArray = [];
+                     //Insert the JSON data to the table view
+                     for(var i=0; i<json.length; i++) {
+                           var row = Ti.UI.createTableViewRow({
+                                  title: json[i].value,
+                                  hasChild : true,
+                           });
+                         dataArray.push(row);
                      };                      
                      $.tableView.setData(dataArray);                            
                }; 
        };   
        function insertData(){
-              //if there is something in the textbox 
+              //if there is something in the textbox
               if($.inserTxtF.value != "" && $.inserTxtF.value != null){
                      var request = Ti.Network.createHTTPClient({
-                  onload:alert("Your chore has been submitted"),
-                  onerror: function(e){ 
-                      Ti.API.debug(e.error); 
-                      alert('There was an error during the conexion'); 
-                  }, 
-                  timeout:1000, 
+                  onload:function(e) {
+                  	alert(this.responseText);
+                  },
+                  onerror: function(e){
+                      Ti.API.debug(e.error);
+                      alert('There was an error during the conexion');
+                  },
+                  timeout:1000,
                      });    
 //Request the data from the web service, Here you have to change it for your local ip 
                      request.open("POST","http://localhost:3000/api/models/"+$.inserTxtF.value); 
                //      var params = ({"id": $.inserTxtF.value});
                //      console.log ('lo que tiene params ', params);
-                  request.send(); 
+                  request.send();
               } 
               else{
                      alert("Please write something in the textbox"); 
